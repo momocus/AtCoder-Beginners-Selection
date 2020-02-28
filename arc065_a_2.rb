01ed22e2670389ef4b9201e55d9fb7fd994f4d9f
momocus <momocus333@gmail.com>
2020-02-28 18:44:37 +0900

    Fix: 白昼夢 dreraseeam 等でYESと誤判定する

--- arc065_a_2.rb
+++ arc065_a_2.rb
@@ -1,6 +1,10 @@
 str = gets
 str.chomp!
-a = str.gsub(/eraser/, "").gsub(/erase/, "").gsub(/dreamer/, "").gsub(/dream/, "")
+a = str.gsub(/eraser/, ",")
+       .gsub(/erase/, ",")
+       .gsub(/dreamer/, ",")
+       .gsub(/dream/, ",")
+       .gsub(",", "")
 if a == ""
   puts "YES"
 else
